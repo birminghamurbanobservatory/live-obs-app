1b93d28304ff76e57c48a59fc01dcc963565a625
SiBell <SiBellUK@gmail.com>
2020-05-28 17:13:59 +0100

    observation feature added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'live-obs-app';
+
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    
+
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ObservationComponent } from './observation/observation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ObservationComponent
   ],
   imports: [
     BrowserModule
